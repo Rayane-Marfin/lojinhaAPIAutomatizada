a78551950f06848f078b198933d2ea9bffbd9295
Rayane Marfin <rayanemarfin@gmail.com>
2023-08-22 15:43:25 -0300

    Inicializando o repositorio com os arquivos iniciais do projeto

---
+++ src/test/java/pojo/UsuarioPojo.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class UsuarioPojo {
+    private String UsuarioLogin;
+    private String UsuarioSenha;
+
+    public String getUsuarioLogin() {
+        return UsuarioLogin;
+    }
+
+    public void setUsuarioLogin(String usuarioLogin) {
+        UsuarioLogin = usuarioLogin;
+    }
+
+    public String getUsuarioSenha() {
+        return UsuarioSenha;
+    }
+
+    public void setUsuarioSenha(String usuarioSenha) {
+        UsuarioSenha = usuarioSenha;
+    }
+}
